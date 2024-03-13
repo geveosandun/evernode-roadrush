a1e0e99f6ae1ce8e6f91fbba1d18d9190c0997bd
Thisuri Bandaranayake <thisuri@geveo.com>
2024-03-13 19:56:59 +0530

    Error fix in controller

--- backend/controller.js
+++ backend/controller.js
@@ -5,7 +5,6 @@ import { TestController } from "./Controllers/Test.Controller";
 import { AuthenticationController } from "./Controllers/Authentication.Controller";
 import { PassengerController } from "./Controllers/Passenger.Controller";
 import { DriverController } from "./Controllers/Driver.Controller";
-import { UserController } from "./Controllers/User.Controller";
 
 const settings = require("./settings.json").settings;
 
@@ -19,51 +18,35 @@ export class Controller {
 	#driverController = null;
 	#userController = null;
 
-	//async handleRequest(user, message, isReadOnly) {
-		async handleRequest() {
-		const message ={
-			Service: 'Driver',
-			Action: 'AcceptRide',
-			Data: {
-				driverID: 1,
-				passengerId: 1,
-				passengerName: 'TestUser',
-				pickUpLocation: 'test',
-				destination: 'test',
-				rideDateTime: 'test',
-				rideRequestId: '1',
-				rideFareAmount: '2.34',
-				rideDistance: '5.8'
-			}
-		  }
-		// this.#contractController = new ContractUpdateController(message);
-		// this.#testController = new TestController(message); // For test purposes
-		// this.#authenticationController = new AuthenticationController(message);
-		// this.#passengerController = new PassengerController(message);
+
+	async handleRequest(user, message, isReadOnly) {
+		this.#contractController = new ContractUpdateController(message);
+		this.#testController = new TestController(message); // For test purposes
+		this.#authenticationController = new AuthenticationController(message);
+		this.#passengerController = new PassengerController(message);
 		this.#driverController = new DriverController(message);
 		this.#userController = new UserController(message);
 		let _authService = new AuthService();
 
 		let result = {};
 
-		// if (message.Service == ServiceTypes.CONTRACT_UPDATE) {
-		// 	const verification = _authService.verifyContractUpdateRequest(message);
-		// 	if (verification.isVerified) {
-		// 		result = await this.#contractController.handleRequest();
-		// 	} else {
-		// 		result = { error: verification.reason ?? "Authentication failed." };
-		// 	}
-		// }
-		//  else {
-			// if (message.Service == ServiceTypes.TEST) {
-			// 	result = await this.#testController.handleRequest();
-			// }
-			// if (message.Service === ServiceTypes.AUTHENTICATION) {
-			// 	result = await this.#authenticationController.handleRequest();
-			// }
-			// if (message.Service === ServiceTypes.PASSENGER) {
-			// 	result = await this.#passengerController.handleRequest();
-			// }
+		if (message.Service == ServiceTypes.CONTRACT_UPDATE) {
+			const verification = _authService.verifyContractUpdateRequest(message);
+			if (verification.isVerified) {
+				result = await this.#contractController.handleRequest();
+			} else {
+				result = { error: verification.reason ?? "Authentication failed." };
+			}
+		} else {
+			if (message.Service == ServiceTypes.TEST) {
+				result = await this.#testController.handleRequest();
+			}
+			if (message.Service === ServiceTypes.AUTHENTICATION) {
+				result = await this.#authenticationController.handleRequest();
+			}
+			if (message.Service === ServiceTypes.PASSENGER) {
+				result = await this.#passengerController.handleRequest();
+			}
 			if (message.Service === ServiceTypes.DRIVER) {
 				result = await this.#driverController.handleRequest();
 			}
@@ -72,14 +55,14 @@ export class Controller {
 			}
 		}
 
-		// if (isReadOnly) {
-		// 	await this.sendOutput(user, result);
-		// } else {
-		// 	await this.sendOutput(
-		// 		user,
-		// 		message.promiseId ? { promiseId: message.promiseId, ...result } : result,
-		// 	);
-		// }
+		if (isReadOnly) {
+			await this.sendOutput(user, result);
+		} else {
+			await this.sendOutput(
+				user,
+				message.promiseId ? { promiseId: message.promiseId, ...result } : result,
+			);
+		}
 	}
 
 	sendOutput = async (user, response) => {
